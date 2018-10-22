dfd1a56bb5db9529c4d350b7ab1fb9bb10e5c3c1
Kolien Pleijsant <42912171+KolienPleijsant@users.noreply.github.com>
2018-10-22 11:19:04 +0200

    Update wikifunction-skeleton.py

--- wikifunction-skeleton.py
+++ wikifunction-skeleton.py
@@ -7,15 +7,19 @@ import requests
 def api_call(title, value):
     url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{title}"
     req = requests.get(url)
-    data = req.json()
     if req.status_code != 200:
         print(f"We got an error: {req.status_code}")
         exit()
-    return data[f"{value}"]
+    return req
+
+def my_function(title, value):
+	data = api_call(title, value).json()
+	return data[f"{value}"]
 
 title = input("Give an article: ").strip()
 value = input("Description or extract? ").strip().lower()
-data = api_call(title, value)
+data = my_function(title, value)
 
+#print(f"https://en.wikipedia.org/wiki/{title}")
 print(f"Here is {value} for {title}:")
 print(data)
